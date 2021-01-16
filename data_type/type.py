2480905d709883671a3bc66eedb21e190b2e1fa9
DESKTOP-MO3DOCD\win <onthicha.s@ku.th>
2021-01-16 13:11:00 +0700

    add new folder

---
+++ data_type/type.py
@@ -0,0 +1,10 @@
+X = "Hello World"
+X = 20
+X = 20.5
+X = ["apple", "banana", "cherry"]
+X = ("apple", "banana", "cherry")
+X = {"name": "onthich", "age":20}
+X = {"apple", "banana", "cherry"}
+X = frozenset({"apple", "banana", "cherry"})
+X = True
+X = b"hello"
